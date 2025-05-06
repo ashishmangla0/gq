978c3b3738146c180f5edd56a92c6ce4907dd29c
ashishmangla <ashishmangla1991@gmail.com>
2025-05-06 15:54:04 +0530

    add new code

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
 import { ApolloServer } from "@apollo/server";
+import {startStandaloneSever} from '@apollo/server/standalone'
+
+const typeDefs = `#graphql
+type Query{
+    gretting: String
+}
+`
 
 
+const resolvers = {
+    Query:{
+        gretting :  ()=> 'Hello ji'
+    }
+}
 const server = new ApolloServer({
-    
+    typeDefs,
+    resolvers
 })
+
+const {url} = await startStandaloneSever(server,{listen:{port:6000}})
